a2bfb06c8488fba5abe66e7f52fa3c3a2a44abde
xiaowei <119865@shfang.net>
2018-07-26 17:04:10 +0800

    core code

--- studyplatform/js/article_home2.js
+++ studyplatform/js/article_home2.js
@@ -54,15 +54,19 @@ app.controller('myCtrl',
                         }
                         $scope.typelists22=typebbbb
                         $scope.articleType=$scope.typelists22[0]
-                        getshuju($scope.page,status, $scope.articleType.type_id)
+                        getshuju($scope.page,status, $scope.articleType.type_id, 1)
                     });
             }
 
             //获取数据api
-            function getshuju(pageID,status,type_id) {
-                $http.get("https://dangjain.ishoubei.com/article?page="+pageID+"&status="+status+"&type_id="+type_id+"&limit=8")
+            function getshuju(pageID,status,type_id, coursetype) {
+                $http.get("https://dangjain.ishoubei.com/article?page="+pageID+"&status="+1+"&type_id="+type_id+"&limit=100")
                     .then(function (res) {
-                        $scope.lists=res.data.rows;
+                        $scope.lists=res.data.rows.filter(function(item){
+                            console.log(item.coursetype)
+                            console.log(coursetype)
+                            return item.coursetype === Number(coursetype)
+                        });
                         $scope.total=res.data.total;
                         $scope.pagesLists=Math.ceil($scope.total/10);
                     });
@@ -87,75 +91,109 @@ app.controller('myCtrl',
             $scope.clickType=function () {
 
                 $scope.newsDangjian=$scope.articleType.type_name
-                getshuju($scope.page,status,$scope.articleType.type_id)
+                getshuju($scope.page,status,$scope.articleType.type_id, 1)
             }
             $scope.onleixingClick1=function () {
                 status=1;
                 $scope.newsDangjian=$scope.articleType.type_name
-                getshuju($scope.page,status,$scope.articleType.type_id)
+                getshuju($scope.page,status,$scope.articleType.type_id, 1)
             }
             $scope.onleixingClick2=function () {
                 status=2;
                 $scope.newsDangjian=$scope.articleType.type_name
-                getshuju($scope.page,status,$scope.articleType.type_id)
+                getshuju($scope.page,status,$scope.articleType.type_id, 2)
             }
             $scope.onleixingClick5=function () {
                 status=5;
                 $scope.newsDangjian=$scope.articleType.type_name
-                getshuju($scope.page,status,$scope.articleType.type_id)
+                getshuju($scope.page,status,$scope.articleType.type_id, 3)
             }
             $scope.downOnClick= function () {
                 if($scope.page<$scope.pagesLists){
                     $scope.page = $scope.page+1;
-                    getshuju($scope.page,status,$scope.articleType.type_id);
+                    getshuju($scope.page,status,$scope.articleType.type_id, 1);
                 }
             }
             $scope.upOnClick= function () {
                 if($scope.page>1) {
                     $scope.page = $scope.page - 1;
-                    getshuju($scope.page,status,$scope.articleType.type_id);
+                    getshuju($scope.page,status,$scope.articleType.type_id, 1);
                 }
             }
 
 
             //选择试题
             $scope.noshitiClick=function (article_id) {
-                $http.get("https://dangjain.ishoubei.com/article/"+article_id+"/question")
+                $http.get("https://dangjain.ishoubei.com/cadre?page=1&limit=1000")
                     .then(function (res) {
-                        $scope.Article_question=res.data.rows
-                        $scope.questionShow=$scope.Article_question.length
-                        for (var i=0;i<$scope.Article_question.length;i++){
-                                yixuanPanduan.push($scope.Article_question[i].eq_id)
-                        }
-                        for(var a=0;a<$scope.shiti1.length;a++){
-                            for(var b=0;b<yixuanPanduan.length;b++){
-                                var tem = yixuanPanduan[b];
-                                $("input:checkbox[value='"+tem+"']").attr("checked","checked");
-
-                            }
-                        }
-
-
+                        $scope.user = res.data.rows
+                        $scope.userLength = $scope.user.length
+                        getStudy(article_id)
                     });
-
-
-
-
-                $scope.xubangdaingId=article_id
-
-
             }
+
             $scope.xuancheClick=function (id) {
-                console.log(id)
-                $http.post("https://dangjain.ishoubei.com/article/"+$scope.xubangdaingId+"/question?eq_id="+id)
-                    .then(function (res) {
-
+                console.log('start')
+                // console.log($scope.removeUserIdList)
+                // console.log($scope.addUserIdList)
+                const item = $scope.selectedUserIdList.find(function(sItem){
+                    return sItem.user_id === id
+                })
+                if(item){
+                    // 有 item - remove
+                    const removeItem = $scope.removeUserIdList.find(function(sItem){
+                        return sItem.user_id === id
                     })
+                    if(removeItem){
+                        //已经添加 - 删除
+                        $scope.removeUserIdList = $scope.removeUserIdList.filter(function(sItem){
+                            return sItem.user_id !== id
+                        })
+                    }
+                    else{
+                        $scope.removeUserIdList.push(item)
+                    }
+                }
+                else { //没有 - 添加
+                    const addItem = $scope.addUserIdList.find(function(sItem){
+                        return sItem.user_id === id
+                    })
+                    if(addItem){
+                        //已经添加 - 删除
+                        $scope.addUserIdList = $scope.addUserIdList.filter(function(sItem){
+                            return sItem.user_id !== id
+                        })
+                    }
+                    else{
+                        $scope.addUserIdList.push({user_id: id})
+                    }
+                }
+                console.log('end')
+                console.log($scope.removeUserIdList)
+                console.log($scope.addUserIdList)
             }
+
             $scope.onshitiClick=function () {
+                //add user
+                for(var i=0; i<$scope.addUserIdList.length; i++){
+                    const data = {
+                        article_id: $scope.xubangdaingId,
+                        user_id: $scope.addUserIdList[i].user_id,
+                        add_user_id: 'xuehaifeng'
+                    }
+                    addUser(data)
+                }
+                //remove user
+                for(var i=0; i<$scope.removeUserIdList.length; i++){
+                    delUser($scope.removeUserIdList[i].us_id)
+                }
                 window.location.href='article_home.html'
             }
 
+            $scope.onCacel=function () {
+                window.location.href='article_home.html'
+            }
+            
 
 
             $scope.deletearticle=function (article_id) {
@@ -268,5 +306,57 @@ app.controller('myCtrl',
                     this[name] = value;
                 }
             }
+        }
+
+        function addUser(data) {
+            console.log('add User')
+            $http({
+                method: 'post',
+                url: 'https://dangjain.ishoubei.com/study',
+                data: $.param(data),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            }).success(function (req) {
+            })
+        }
+
+        function delUser(us_id){
+            console.log('del User')
+            $http.delete('https://dangjain.ishoubei.com/study/' + us_id)
+            .then(function (req) {
+            })
+        }
+
+        function getStudy(article_id) {
+            console.log('getStudy by article_id')
+            $http.get("https://dangjain.ishoubei.com/study?article_id="+article_id + "&limit=1000")
+            .then(function (res) {
+                $scope.selectedUserIdList = res.data.rows.map(function(item){
+                    return {
+                        user_id: item.user_id,
+                        article_id: item.article_id,
+                        us_id: item.us_id
+                    }
+                }) //TODO
+                console.log($scope.selectedUserIdList)
+                $scope.removeUserIdList = [] //TODO
+                $scope.addUserIdList = [] //TODO
+                $scope.xubangdaingId=article_id
+                $scope.user = $scope.user.map(function(item){
+                    const selectedItem = $scope.selectedUserIdList.find(function(sItem){
+                        return sItem.user_id === item.user_id
+                    })
+                    console.log('-----')
+                    console.log(selectedItem)
+                    console.log('-----')
+                    if(selectedItem){
+                        item.isSelected = true
+                    }
+                    else{
+                        item.isSelected = false
+                    }
+                    return item
+                })
+                console.log($scope.user)
+            })
         }
     });
